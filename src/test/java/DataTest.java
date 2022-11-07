e9209e3587b99bb82410f41112f4918d335b789b
Robert <s2roroth@uni-trier.de>
2022-11-07 14:49:10 +0100

    Fixed Bad Test.

--- src/test/java/DataTest.java
+++ src/test/java/DataTest.java
@@ -20,6 +20,6 @@ public class DataTest {
       DataLoader dataLoader = new DataLoader();
       DataParser dataParser = new DataParser();
       String json = dataLoader.loadJsonFile("countries-aggregated_json.json");
-      Assert.assertTrue(dataParser.parseJsonData(json).isEmpty());
+      Assert.assertFalse(dataParser.parseJsonData(json).isEmpty());
     }
 }
